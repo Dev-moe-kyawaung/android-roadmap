0cd07fe6521f07f286d292505b29df5c045d9bd8
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-08-05 18:28:43 +0630

    Add RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { siteMetadata } from "@/components/seo";
+
+export const metadata: Metadata = {
+  ...siteMetadata
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
